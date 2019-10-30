65fd1489e9257d25c926e4b0c5b338ff6c272048
Alan <alansamuel94@gmail.com>
2019-10-30 23:16:28 +1100

    Finished setting up base GraphQL queries and mutations

--- Resolvers/rootResolver.js
+++ Resolvers/rootResolver.js
@@ -1,9 +1,37 @@
+const testAddress = {
+    streetNumber: 4,
+    streetName: "Madge Street",
+    suburb: "Blair Athol",
+    state: "SA",
+    country: "Australia"
+}
+const testEvent = {
+    name: "Photoshoot",
+    date: "12/06/2010",
+    address: testAddress
+}
+
+const eventArray = [
+    testEvent,testEvent
+]
+
 // Provide resolver functions for your schema fields
 const resolvers = {
     Query: {
-      hello: (root, args, context) => {
-        return 'Hello world!'
+        getEvents: (parent, args, context) => {
+        return eventArray
       }
+    },
+    Mutation: {
+        addEvent:(parent, args) =>{
+            const newEvent = {
+                name: args.name,
+                date: args.date,
+                address: args.input
+            }
+            eventArray.push(newEvent)
+            return eventArray
+        }
     }
   }
 
--- Schema/rootSchema.js
+++ Schema/rootSchema.js
@@ -2,8 +2,35 @@ const { gql } = require('apollo-server');
 
 const rootSchema = gql`
     type Query {
-        hello: String
+        getEvents: [Event]
     }
+
+    type Mutation {
+        addEvent(name: String, date: String, input: AddressInput): [Event]
+    }
+
+    input AddressInput {
+        streetNumber: Int,
+        streetName: String,
+        suburb: String,
+        state: String,
+        country: String
+    }
+
+    type Event{
+        name: String,
+        date: String,
+        address: Address
+    }
+
+    type Address{
+        streetNumber: Int,
+        streetName: String,
+        suburb: String,
+        state: String,
+        country: String
+    }
+    
 `;
 
 module.exports = rootSchema;
