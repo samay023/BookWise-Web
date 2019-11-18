95b0255d5ce8d415f60eaed4544f619895b4d784
Alan <alansamuel94@gmail.com>
2019-11-18 20:21:49 +1100

    Added active classes for buttons

--- client/src/components/layouts/Session/SessionList.js
+++ client/src/components/layouts/Session/SessionList.js
@@ -1,61 +1,48 @@
 import { Container, Row, Col} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
 import "./Session.css";
 
 const SessionList = () => {
+    const [row, activeRow] = useState({
+        key: "1",
+        selected : true
+    });
+
+    const setActiveRow = (event) => {
+        activeRow({key: event.currentTarget.id,selected:true});
+    };
+
   return (
     <Container className="CurrentSessions">
     <div>
       <Row className="title">December, 2019</Row>
-      <Row className="SessionDetails" active="true">
+      <Row id="1" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "1" ? 'active' : ''}`}>
           <Col className="Date">12 - Thu</Col>
           <Col>Birthday Shoot</Col>
       </Row>
-      <Row className="SessionDetails">
+      <Row id="2" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "2" ? 'active' : ''}`}>
           <Col className="Date">15 - Sun</Col>
           <Col>Indoor shoot</Col>
       </Row>
     </div>
     <div>
         <Row className="title">January, 2020</Row>
-        <Row className="SessionDetails">
+        <Row id="3" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "3" ? 'active' : ''}`}>
             <Col className="Date">01 - Mon</Col>
             <Col>Birthday Shoot</Col>
         </Row>
-        <Row className="SessionDetails">
+        <Row id="4" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "4" ? 'active' : ''}`}>
             <Col className="Date">30 - Tues</Col>
             <Col>Indoor shoot</Col>
         </Row>
     </div>
     <div>
         <Row className="title">Februray, 2020</Row>
-        <Row className="SessionDetails">
+        <Row id="5" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "5" ? 'active' : ''}`}>
             <Col className="Date">15 - Wed</Col>
             <Col>Birthday Shoot</Col>
         </Row>
-        <Row className="SessionDetails">
-            <Col className="Date">25 - Fri</Col>
-            <Col>Indoor shoot</Col>
-        </Row>
-      </div>
-      <div>
-        <Row className="title">March, 2020</Row>
-        <Row className="SessionDetails">
-            <Col className="Date">15 - Wed</Col>
-            <Col>Birthday Shoot</Col>
-        </Row>
-        <Row className="SessionDetails">
-            <Col className="Date">25 - Fri</Col>
-            <Col>Indoor shoot</Col>
-        </Row>
-      </div>
-      <div>
-        <Row className="title">April, 2020</Row>
-        <Row className="SessionDetails">
-            <Col className="Date">15 - Wed</Col>
-            <Col>Birthday Shoot</Col>
-        </Row>
-        <Row className="SessionDetails">
+        <Row id="6" onClick={setActiveRow.bind(this)} className={`SessionDetails ${row.key === "6" ? 'active' : ''}`}>
             <Col className="Date">25 - Fri</Col>
             <Col>Indoor shoot</Col>
         </Row>
